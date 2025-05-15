26de6904b07e1fec9cf84b250fde1fdc336450d9
Scott Erickson <theerickson@gmail.com>
2025-05-15 12:16:39 -0600

    add error handling for a disconnected network

--- app/controllers/genius_controller.rb
+++ app/controllers/genius_controller.rb
@@ -5,27 +5,32 @@ class GeniusController < ApplicationController
   API_URI = 'https://api.genius.com'
 
   def index
-    if params[:artist_id].present?
-      get_songs(params[:artist_id].to_i, (params[:page].present? ? params[:page].to_i : 1))
-    elsif params[:query].present?
-      @query = params[:query]
-      uri = URI(API_URI)
-      uri.path = '/search'
-      uri.query = URI.encode_www_form({ q: @query })
-      response = Net::HTTP.get(uri, api_headers)
-      json_response = JSON.parse response
-      artists = Set.new
-      json_response['response']['hits'].each do |hit|
-        hit['result']['primary_artists'].each do |artist|
-          artists << [ artist['name'], artist['id'] ]
+    begin
+      if params[:artist_id].present?
+        get_songs(params[:artist_id].to_i, (params[:page].present? ? params[:page].to_i : 1))
+      elsif params[:query].present?
+        @query = params[:query]
+        uri = URI(API_URI)
+        uri.path = '/search'
+        uri.query = URI.encode_www_form({ q: @query })
+        response = Net::HTTP.get(uri, api_headers)
+        json_response = JSON.parse response
+        artists = Set.new
+        json_response['response']['hits'].each do |hit|
+          hit['result']['primary_artists'].each do |artist|
+            artists << [ artist['name'], artist['id'] ]
+          end
+        end
+
+        if artists.size != 1
+          @artists = artists
+        else
+          get_songs artists.first.second
         end
       end
-
-      if artists.size != 1
-        @artists = artists
-      else
-        get_songs artists.first.second
-      end
+    rescue StandardError => e
+      logger.error e.message
+      @error = true
     end
   end
 
